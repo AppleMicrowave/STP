728075f21828d012505fd02ecc5c5d0ec010a32b
ShidaraKidd <vipescol@mail.ru>
2025-12-09 16:36:05 +0700

    add lab10

---
+++ Lab.Tests/Test10.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab10;
+
+namespace Lab10.Tests
+{
+    [TestClass]
+    public class PolynomialMultiplicationTests
+    {
+        [TestMethod]
+        public void Multiply_ZeroPolynomials_ReturnsZero()
+        {
+            var p1 = new TPoly();
+            var p2 = new TPoly();
+            var result = p1.Multiply(p2);
+            Assert.AreEqual("0*X^0", result.ToString());
+            Assert.AreEqual(0, result.Degree());
+        }
+
+        [TestMethod]
+        public void Multiply_ZeroAndNonZero_ReturnsZero()
+        {
+            var p1 = new TPoly();
+            var p2 = new TPoly(5, 3);
+            var result = p1.Multiply(p2);
+            Assert.AreEqual("0*X^0", result.ToString());
+        }
+
+        [TestMethod]
+        public void Multiply_Constants_ReturnsConstant()
+        {
+            var p1 = new TPoly(3, 0);
+            var p2 = new TPoly(4, 0);
+            var result = p1.Multiply(p2);
+            Assert.AreEqual("12*X^0", result.ToString());
+        }
+
+        [TestMethod]
+        public void Multiply_LinearPolynomials_ReturnsQuadratic()
+        {
+            var p1 = new TPoly(2, 1); // 2x
+            var p2 = new TPoly(3, 1); // 3x
+            var result = p1.Multiply(p2); // 6x²
+            Assert.AreEqual("6*X^2", result.ToString());
+        }
+
+        [TestMethod]
+        public void Multiply_PolynomialWithDifferentDegrees()
+        {
+            var p1 = new TPoly(2, 1).Add(new TPoly(3, 2)); // 3x² + 2x
+            var p2 = new TPoly(1, 0).Add(new TPoly(4, 1)); // 4x + 1
+
+            var result = p1.Multiply(p2);
+            // (3x² + 2x)(4x + 1) = 12x³ + 3x² + 8x² + 2x = 12x³ + 11x² + 2x
+            Assert.AreEqual("2*X^1 + 11*X^2 + 12*X^3", result.ToString());
+        }
+
+        [TestMethod]
+        public void Multiply_Binomials_ReturnsDifferenceOfSquares()
+        {
+            var p1 = new TPoly(1, 0).Add(new TPoly(1, 1)); // x + 1
+            var p2 = new TPoly(1, 0).Add(new TPoly(-1, 1)); // -x + 1
+
+            var result = p1.Multiply(p2); // (x+1)(1-x) = -x² + 1
+            Assert.AreEqual("1*X^0 + -1*X^2", result.ToString());
+        }
+
+        [TestMethod]
+        public void Multiply_PolynomialWithZeroMembers_IgnoresZeros()
+        {
+            var p1 = new TPoly(2, 1).Add(new TPoly(0, 5));
+            var p2 = new TPoly(3, 2);
+
+            var result = p1.Multiply(p2); // (2x)(3x²) = 6x³
+            Assert.AreEqual("6*X^3", result.ToString());
+        }
+
+        [TestMethod]
+        public void Multiply_CommutativeProperty()
+        {
+            var p1 = new TPoly(2, 3).Add(new TPoly(5, 1));
+            var p2 = new TPoly(3, 2).Add(new TPoly(4, 0));
+
+            var result1 = p1.Multiply(p2);
+            var result2 = p2.Multiply(p1);
+
+            Assert.AreEqual(result1.ToString(), result2.ToString());
+            Assert.IsTrue(result1.Equals(result2));
+        }
+
+        [TestMethod]
+        public void Multiply_AssociativeProperty()
+        {
+            var p1 = new TPoly(2, 1);
+            var p2 = new TPoly(3, 1);
+            var p3 = new TPoly(4, 1);
+
+            var result1 = p1.Multiply(p2).Multiply(p3);
+            var result2 = p1.Multiply(p2.Multiply(p3));
+
+            Assert.AreEqual(result1.ToString(), result2.ToString());
+        }
+
+        [TestMethod]
+        public void Multiply_DistributiveProperty()
+        {
+            var p1 = new TPoly(2, 1);
+            var p2 = new TPoly(3, 1);
+            var p3 = new TPoly(4, 1);
+
+            var left = p1.Multiply(p2.Add(p3));
+            var right = p1.Multiply(p2).Add(p1.Multiply(p3));
+
+            Assert.AreEqual(left.ToString(), right.ToString());
+        }
+
+        [TestMethod]
+        public void Multiply_HighDegreePolynomials()
+        {
+            var p1 = new TPoly(5, 10);
+            var p2 = new TPoly(3, 5);
+
+            var result = p1.Multiply(p2); // 15x¹⁵
+            Assert.AreEqual("15*X^15", result.ToString());
+            Assert.AreEqual(15, result.Degree());
+        }
+    }
+}
---
+++ lab10/TPoly.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab10
+{
+    public class TMember
+    {
+        public int Coeff { get; set; }
+        public int Degree { get; set; }
+
+        public TMember(int coeff = 0, int degree = 0)
+        {
+            Coeff = coeff;
+            Degree = degree;
+        }
+
+        public bool Equals(TMember other)
+        {
+            return Coeff == other.Coeff && Degree == other.Degree;
+        }
+
+        public TMember Differentiate()
+        {
+            if (Degree == 0)
+                return new TMember(0, 0);
+            return new TMember(Coeff * Degree, Degree - 1);
+        }
+
+        public double Evaluate(double x)
+        {
+            return Coeff * Math.Pow(x, Degree);
+        }
+
+        public override string ToString()
+        {
+            return $"{Coeff}*X^{Degree}";
+        }
+    }
+
+    public class TPoly
+    {
+        private List<TMember> members = new List<TMember>();
+
+        public TPoly(int coeff = 0, int degree = 0)
+        {
+            if (coeff != 0)
+                members.Add(new TMember(coeff, degree));
+        }
+
+        public int Degree()
+        {
+            if (!members.Any()) return 0;
+            return members.Where(m => m.Coeff != 0).Max(m => m.Degree);
+        }
+
+        public int Coeff(int degree)
+        {
+            var member = members.FirstOrDefault(m => m.Degree == degree);
+            return member?.Coeff ?? 0;
+        }
+
+        public void Clear()
+        {
+            members.Clear();
+        }
+
+        public TPoly Add(TPoly other)
+        {
+            var result = new TPoly();
+            result.members.AddRange(this.members);
+            result.members.AddRange(other.members);
+            result.Normalize();
+            return result;
+        }
+
+        public TPoly Subtract(TPoly other)
+        {
+            return this.Add(other.Negate());
+        }
+
+        public TPoly Multiply(TPoly other)
+        {
+            var result = new TPoly();
+            foreach (var m1 in this.members)
+                foreach (var m2 in other.members)
+                    result.members.Add(new TMember(m1.Coeff * m2.Coeff, m1.Degree + m2.Degree));
+            result.Normalize();
+            return result;
+        }
+
+        public TPoly Negate()
+        {
+            var result = new TPoly();
+            foreach (var m in this.members)
+                result.members.Add(new TMember(-m.Coeff, m.Degree));
+            result.Normalize();
+            return result;
+        }
+
+        public bool Equals(TPoly other)
+        {
+            this.Normalize();
+            other.Normalize();
+            if (this.members.Count != other.members.Count) return false;
+            for (int i = 0; i < this.members.Count; i++)
+                if (!this.members[i].Equals(other.members[i]))
+                    return false;
+            return true;
+        }
+
+        public TPoly Differentiate()
+        {
+            var result = new TPoly();
+            foreach (var m in this.members)
+            {
+                var diff = m.Differentiate();
+                if (diff.Coeff != 0)
+                    result.members.Add(diff);
+            }
+            result.Normalize();
+            return result;
+        }
+
+        public double Evaluate(double x)
+        {
+            return members.Sum(m => m.Evaluate(x));
+        }
+
+        public TMember GetMember(int index)
+        {
+            if (index < 0 || index >= members.Count)
+                throw new IndexOutOfRangeException();
+            return members[index];
+        }
+
+        public int MemberCount()
+        {
+            return members.Count;
+        }
+
+        private void Normalize()
+        {
+            RemoveZeros();
+            SortByDegree();
+            CombineLikeTerms();
+        }
+
+        private void RemoveZeros()
+        {
+            members.RemoveAll(m => m.Coeff == 0);
+        }
+
+        private void SortByDegree()
+        {
+            members = members.OrderBy(m => m.Degree).ToList();
+        }
+
+        private void CombineLikeTerms()
+        {
+            if (!members.Any()) return;
+            var result = new List<TMember>();
+            TMember current = members[0];
+
+            for (int i = 1; i < members.Count; i++)
+            {
+                if (members[i].Degree == current.Degree)
+                {
+                    current.Coeff += members[i].Coeff;
+                }
+                else
+                {
+                    if (current.Coeff != 0)
+                        result.Add(current);
+                    current = members[i];
+                }
+            }
+            if (current.Coeff != 0)
+                result.Add(current);
+            members = result;
+        }
+
+        public override string ToString()
+        {
+            if (!members.Any()) return "0*X^0";
+            return string.Join(" + ", members.Select(m => m.ToString()));
+        }
+    }
+}
